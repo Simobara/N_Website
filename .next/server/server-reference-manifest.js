76d1a502c7853b9438ae635fe09840e6cf3eb7eb
SimonBara <simobara@hotmail.it>
2024-07-16 16:35:23 +0200

    Remove all references to fraction.js and update dependencies

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"WjgA6JjUNnoagR6fA/kEUANCl7Ieef+2CRurucCWKIk=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"05BpaNzkXqMypciBL3p1AafTf++9aXaimjDj58C9rZc=\"}"
